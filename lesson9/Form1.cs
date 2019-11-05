7700a4650ec923ad98d84253d05c2f4af9d71275
igorBobrovA <57355113+igorBobrovA@users.noreply.github.com>
2019-11-05 18:55:50 +0700

    v2

--- lesson9/Form1.cs
+++ lesson9/Form1.cs
@@ -16,5 +16,39 @@ namespace lesson9
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            Random R = new Random();
+            int[,] mass= new int[5,3];
+            int max = 0,
+                flag = 0,
+                sum = 0;
+            for(int i = 0; i < 5; i++)
+            {
+                sum = 0;
+                for(int j = 0; j < 3; j++)
+                {
+                    mass[i, j] = R.Next(10,100);
+                    richTextBox1.Text += mass[i,j] + "    ";
+                    sum += mass[i,j];
+                }
+                if(sum > max)
+                {
+                    max = sum;
+                    flag = i;
+                    
+                }
+                
+                richTextBox1.Text += "\n";
+            }
+            
+            richTextBox1.Text += "максимальная сумма в " + (flag+1) + " строке \n";
+            for (int j = 0; j < 3; j++)
+            {
+                richTextBox1.Text += mass[flag,j] + " ";
+            }
+        }
     }
 }
